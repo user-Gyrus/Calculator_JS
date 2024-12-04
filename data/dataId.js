fa622bebde6ca06f8ab0159d8cc8f18e3ec78cc6
user_Gyrus <nandakumarblj@gmail.com>
2024-12-05 00:26:18 +0530

    Added the new file for the data, imported the required arrays into the main js file

---
+++ data/dataId.js
@@ -0,0 +1,57 @@
+const numbers = [
+    {
+        id: 'one',
+        value: 1
+    },
+    {
+        id: 'two',
+        value: 1
+    },
+    {
+        id: 'three',
+        value: 1
+    },
+    {
+        id: 'four',
+        value: 1
+    },
+    {
+        id: 'five',
+        value: 1
+    },
+    {
+        id: 'six',
+        value: 1
+    },
+    {
+        id: 'seven',
+        value: 1
+    },
+    {
+        id: 'eight',
+        value: 1
+    },
+    {
+        id: 'nine',
+        value: 1
+    }
+];
+
+const operators = [
+    {
+        id: 'multiply',
+        symbol: '*'
+    },
+    {
+        id: 'divide',
+        symbol: '/'
+    },
+    {
+        id: 'add',
+        symbol: '+'
+    },
+    {
+        id: 'subtract',
+        symbol: '-'
+    }
+];
---
+++ scripts/page.js
@@ -0,0 +1,7 @@
+import {numbers, operators} from '../data/dataId';
+
+let htmlContent = '';
+
+
+
+document.querySelector('.header_area').innerHTML = htmlContent;
